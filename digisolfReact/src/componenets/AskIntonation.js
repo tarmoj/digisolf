f27f2c0d4fa304e08fb1569df36875b1244d231d
Tarmo Johannes <tarmo@otsakool.edu.ee>
2020-04-10 00:33:15 +0300

    First almost-working version of AskIntonation.

--- digisolfReact/src/componenets/AskIntonation.js
+++ digisolfReact/src/componenets/AskIntonation.js
@@ -18,83 +18,171 @@ const AskIntonation = () => {
     const dispatch = useDispatch();
 
     const name = useSelector(state => state.exerciseReducer.name);
-    const [exerciseHasBegun, setExerciseHasBegun] = useState(false);
-    const [possibleChords, setPossibleChords] = useState([]);
-    const [selectedChord, setSelectedChord] = useState([]);
-    const [answer, setAnswer] = useState(null);
+    const isHarmonic = useSelector(state => state.exerciseReducer.isHarmonic);
+    // deviation in Cents -  get from state or exercise name...
 
+    const [exerciseHasBegun, setExerciseHasBegun] = useState(false);
+    const [intervalRatio, setIntervalRatio] = useState(1.5);
+    const [answer, setAnswer] = useState(null);
+    const [baseMidiNote, setBaseMidiNote] = useState(60);
+    const [selectedDeviation, setSelectedDeviation] = useState(0);
+
+    const possibleDeviations = [5, 10, 20, 30];
+    const possibleIntervalRatios = [4/3, 3/2, 2];
+
+    let deviation;
 
     // EXERCISE LOGIC ======================================
 
     const startExercise = () => {
         setExerciseHasBegun(true);
-        // what is right place for setting the volume?
 
+        // this does not work here. Should wait until csound script is loaded:
+        csoundStart();
+        let cents = 0;
+        // TODO: get deviation from exercise name
         switch(name) {
             case "a":
-
+                    cents = 30;
                 break;
             case "b":
-
+                    cents = 20;
+                break;
+            case "c":
+                cents = 10;
+                break;
+            case "d":
+                cents = 5;
                 break;
             default:
-                console.log("no exercise found");
-                return;
+                cents = 0;
         }
-
-        //setPossibleChords(possibleChords);
-        csoundTest();
-
-        renew();
-
-
+        renew(cents);
     };
 
 
-    const csoundTest = () => {
-        const csound = window.csound; //new CsoundObj();  //require('csound-wasm');
-        console.log("Csound type: ", typeof(csound));
-        if (typeof (csound)==="undefined") {
-            console.log("Csound ins undefined");
+
+    const csoundStart = () => {
+        console.log("*** CSOUND START STARTED ***")
+        const csound = window.csound;
+        console.log("Csound types: ", typeof(csound), typeof(cs));
+
+        if (typeof(csound) === "undefined") {
+            console.log("Csound is not ready yet!");
             return;
         }
 
+        if (csound.get0dbfs() === 1 ) {
+            console.log("Orchestra seems to be already compiled.");
+            return;
+        }
+
+        // does not make sense:
+        // csound.on("ready", async () => {console.log("Csound is ready now."); });
 
 
-        const beeper = `
-instr 1
-  asig = poscil:a(0.3, 440)
-  outc asig, asig
-endin`;
+        const csoundOrchestra = `
+giSine ftgen 1,0, 16384, 10, 1 ; Sine
+giSawtooth ftgen 2,0,  16384, 10, 1, 0.5, 0.3, 0.25, 0.2, 0.167, 0.14, 0.125, .111   ; Sawtooth
+giSquare ftgen 3,  0, 16384, 10, 1, 0,   0.3, 0,    0.2, 0,     0.14, 0,     .111   ; Square
 
-        const makeBeep = `i 1 0 2`;
+; parameters from p4 -  amp, midinote, intervalRatio, cents, soundtype (1- sine, 2 - saw, 3- square), isMelodic (1|0)
+instr PlayInterval
+	iAmp = (p4==0) ? 0.3 : p4
+	iFreq1 = cpsmidinn(p5) ; pich given as midi note
+	iIntervalRatio = p6 ; frequency ratio from base note 1.5 - perfect fifth etc
+	iCents = p7 ; deviation in cents, positive or negative 
+	iFreq2 = iFreq1 * iIntervalRatio * cent(iCents)
+	iSoundType = p8
+	iPlayMelodic = p9
+	iTable = (iSoundType >0 && iSoundType <=3) ? iSoundType : 1 
+	 
+	
+	iDuration = (iPlayMelodic>0) ? p3/2 : p3
+	iStart2 = (iPlayMelodic>0) ? iDuration : 0
+	; think how to use samples of instruments
+	iInstrument = nstrnum("Beep")
+	
+	schedule iInstrument, 0, iDuration, iAmp, iFreq1, iTable
+	schedule iInstrument, iStart2, iDuration, iAmp, iFreq2,iTable 
+endin
 
+
+instr Beep
+	iAmp = p4
+	iFreq = p5
+	iTable = p6	
+	aEnvelope linen iAmp, 0.1, p3, 0.5
+	aSignal poscil aEnvelope, iFreq, iTable
+	outs aSignal, aSignal
+endin
+
+    `;
+        csound.setOption("-m 0");
+        csound.setOption("-d");
         csound.startRealtime();
-        csound.compileOrc(beeper);
-        console.log("dbfs: ", csound.get0dbfs());
-        csound.readScore(makeBeep);
+        csound.compileOrc(csoundOrchestra);
 
-        //setTimeout(() => process.exit(), 5000);
+        console.log("dbfs2: ", csound.get0dbfs());
+        const makeBeep = `i "Beep" 0 2 0.1 440 2`;
 
     };
 
+
+
+
     // renew generates answer and performs play/show
-    const renew = () =>  {
-        //const midiNote = getRandomInt(53, 72); // TODO: make the range configurable
-        //setBaseMidiNote(midiNote);
-        //const selectedChord = getRandomElementFromArray(possibleChords);
-        //setSelectedChord(selectedChord);
-        //console.log("Selected chord: ", t(selectedChord.longName), baseMidiNote, midiNote );
-        const answer = {shortName: selectedChord.shortName}; // may be different in different exercised, might need switch/case
-        setAnswer(answer);
-        const interval = 1.5, deviation = -30;
-        play(interval, deviation);
+    const renew = (cents) =>  {
+
+        const deviationAmount =  (cents === 0) ? getRandomElementFromArray(possibleDeviations) : cents; // the number of cents the interval can be wrong
+
+        const random = Math.random()*3;
+        let deviation, intonation;
+        if (random < 1) {
+            deviation = -deviationAmount;
+            intonation = "low";
+        } else if (random > 2) {
+            deviation = deviationAmount;
+            intonation = "high";
+        } else {
+            deviation = 0;
+            intonation = "inTune";
+        }
+        setSelectedDeviation(deviation);
+        setAnswer({intonation: intonation});
+
+        const midiNote = getRandomInt(53, 72); // TODO: make the range configurable
+        setBaseMidiNote(midiNote);
+        const intervalRatio = getRandomElementFromArray(possibleIntervalRatios);
+        setIntervalRatio(intervalRatio);
+
+        play(midiNote, intervalRatio, deviation);
     };
 
     // what is more generic name -  perform? execute? present?
-    const play = (interval, deviation) => {
+    const play = (midiNote=60, intervalRatio=1.5, deviation=0) => {
+        console.log("*** PLAY*** ");
         const duration = 4; // TODO: make configurable
-        csoundTest();
+        const csound = window.csound;
+        csoundStart();
+        if (typeof(csound) !== "undefined") { // csound is in global space
+            if (csound.get0dbfs()>1) {  // clumsy way to check if orchestra is compiled
+                csoundStart();
+            }
+            // csound instrument 1 (PlayInterval) parameters from p4 on:
+            // amp, midinote, intervalRatio, cents, soundtype (1- sine, 2 - saw, 3- square), isMelodic (1|0)
+            const volume = 0.3 ; // TODO: from parameters
+            let csoundString = 'i 1 0 2 ';
+            const soundType = 2;
+            csoundString += volume + ' ';
+            csoundString += midiNote + ' ';
+            csoundString += intervalRatio + ' ';
+            csoundString += deviation + ' ';
+            csoundString +=  soundType +  ' '; // soundtype: 1- sine, 2 - saw, 3- square
+            csoundString += isHarmonic ? ' 0 ' : ' 1 '; // melodic: 1- yes, 0 - harmonic
+            console.log(csoundString);
+            window.csound.readScore(csoundString);
+        }
 
     };
 
@@ -121,14 +209,16 @@ endin`;
         console.log("Begun: ", exerciseHasBegun);
         // console.log("Begun: ", exerciseHasBegun());
         // if (exerciseHasBegun()) {
+
+        // NB! MAth.abs(deviation) is wrong! should be cents -  put it to state
         if (exerciseHasBegun) {
             return (
                 <Grid.Row  columns={2} centered={true}>
                     <Grid.Column>
-                        <Button color={"green"} onClick={() => renew(possibleChords)} className={"fullWidth marginTopSmall"} >{t("playNext")}</Button>
+                        <Button color={"green"} onClick={() => renew(Math.abs(deviation))} className={"fullWidth marginTopSmall"} >{t("playNext")}</Button>
                     </Grid.Column>
                     <Grid.Column>
-                        <Button /*onClick={() => play(selectedChord, baseMidiNote)}*/ className={"fullWidth marginTopSmall"}  >{t("repeat")}</Button>
+                        <Button onClick={() => play(baseMidiNote, intervalRatio, selectedDeviation)} className={"fullWidth marginTopSmall"}  >{t("repeat")}</Button>
                     </Grid.Column>
                 </Grid.Row>
 
@@ -137,7 +227,7 @@ endin`;
             return(
                 <Grid.Row  >
                     <Grid.Column>
-                    <Button color={"green"} onClick={startExercise} className={"fullWidth marginTopSmall"}>{t("startExercise")}</Button>
+                    <Button color={"green"} onClick={() => startExercise()} className={"fullWidth marginTopSmall"}>{t("startExercise")}</Button>
                     </Grid.Column>
                 </Grid.Row>
             );
@@ -145,21 +235,23 @@ endin`;
     };
 
     const createResponseButtons = () => {
-        let rows = [];
-
-        for (let i = 0, n = possibleChords.length; i < n; i += 2) {
-            const row = createResponseButtonRow(possibleChords[i], possibleChords[i + 1]);
-            rows.push(row);
-        }
-
-        return rows;
-    };
-
-    const createResponseButtonRow = (chord1, chord2) => {
         return (
-            <Grid.Row columns={2}>
-                {createResponseButtonColumn(chord1)}
-                {createResponseButtonColumn(chord2)}
+            <Grid.Row columns={3}>
+                <Grid.Column>
+                    <Button className={"exerciseBtn"}
+                            onClick={() => checkResponse({intonation: "low"})}>{t("low")}
+                    </Button>
+                </Grid.Column>
+                <Grid.Column>
+                    <Button className={"exerciseBtn"}
+                            onClick={() => checkResponse({intonation: "inTune"})}>{t("inTune")}
+                    </Button>
+                </Grid.Column>
+                <Grid.Column>
+                    <Button className={"exerciseBtn"}
+                            onClick={() => checkResponse({intonation: "high"})}>{t("high")}
+                    </Button>
+                </Grid.Column>
             </Grid.Row>
         )
     };
@@ -174,11 +266,11 @@ endin`;
     };
 
 
+
     return (
         <div>
             <Helmet>
-                <script src="https://github.com/hlolli/csound-wasm/releases/download/6.12.0-5/csound-wasm-browser.js">
-                </script>
+                <script type="text/javascript" src="https://github.com/hlolli/csound-wasm/releases/download/6.12.0-5/csound-wasm-browser.js"></script>
             </Helmet>
             <Header size='large'>{`${t(name)} `}</Header>
             <Grid>
