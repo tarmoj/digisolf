3a0f3135dbd233f3d9bd56cb174d4926ac83545c
Tarmo Johannes <tarmo@otsakool.edu.ee>
2020-06-08 21:22:38 +0300

    Added general index to exercises

---
+++ digisolfReact/src/keymap.js
@@ -0,0 +1,11 @@
+export default {
+  AskChord: {
+    CHORD1: 'control+1',
+    CHORD2: 'control+2',
+    NEW: {
+      osx: ['command+n'],
+      windows: 'control+n',
+      linux: 'control+n',
+    },
+  },
+} 
