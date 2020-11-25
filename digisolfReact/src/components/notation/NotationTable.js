ef389f34e08b2f300b5531a0f59ce13c5bef9f32
Jane Tereping <janethecreative@gmail.com>
2020-11-25 18:21:26 +0200

    make the showing of NotationTable conditional

--- digisolfReact/src/components/notation/NotationTable.js
+++ digisolfReact/src/components/notation/NotationTable.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import {Table, Image, Button, Popup} from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Table, Image, Button, Popup, Accordion, Icon} from 'semantic-ui-react';
 import {vtNames, octaveData} from './notationConstants';
 
 const NotationTable = ({addNote, removeNote, selected, setters}) => {
+
+  const [showTable, setShowTable] = useState(false);
+  const [iconClass, setIconClass] = useState("iconDown");
 
   const onNoteClick = name => {
     setters.setNote(name);
@@ -40,6 +43,20 @@ const NotationTable = ({addNote, removeNote, selected, setters}) => {
     }
   }
 
+  // this is to get a fancy animation when accordionblock's opening
+  const onTitleClick = () => {
+    toggleTable();
+    if (iconClass === "iconDown") {
+      setIconClass("iconUp");
+    } else {
+      setIconClass("iconDown");
+    }
+  }
+
+  const toggleTable = () => {
+    setShowTable(!showTable);
+  }
+
   const isNoteSelected = name => {
     return name === selected.note;
   }
@@ -61,53 +78,60 @@ const NotationTable = ({addNote, removeNote, selected, setters}) => {
   }
 
   return(
-    <React.Fragment>
-      <Table unstackable >
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell textAlign='center' width='2' />
-            <NotationTableCell name={'whole'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
-            <NotationTableCell name={'half'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
-            <NotationTableCell name={'quarter'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
-            <NotationTableCell name={'eighth'} handleClick={onNoteDurationClick} checkIfSelected={(isNoteDurationSelected)} />
-            <NotationTableCell name={'sixteenth'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
-            <NotationTableCell name={'dot'} handleClick={onDotClick} checkIfSelected={isDotSelected} />
-          </Table.Row>
-          <Table.Row>
-            <NotationTableCell name={'C'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'D'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'E'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'F'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'G'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'A'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'B'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
-            <NotationTableCell name={'rest'} handleClick={onRestClick} checkIfSelected={isRestSelected} />
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell textAlign='center' width='2' />
-          <NotationTableCell name={'dblflat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
-          <NotationTableCell name={'flat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
-          <NotationTableCell name={'nat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
-          <NotationTableCell name={'sharp'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
-          <NotationTableCell name={'dblsharp'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
-          <Table.Cell textAlign='center' width='2' />
-          <Table.Cell textAlign='center' width='2' />
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell textAlign='center' width='2' />
-          <Table.Cell textAlign='center' width='2' />
-          <Table.Cell textAlign='center' width='2' />
-          <NotationTableCell name={'octaveup'} handleClick={onOctaveUpClick} popupContent={'Oktav kõrgemaks'} />
-          <NotationTableCell name={'octavedown'} handleClick={onOctaveDownClick} popupContent={'Oktav madalamaks'} />
-          <Table.Cell textAlign='center' width='2' />
-          <Table.Cell textAlign='center' width='2' />
-          <Table.Cell textAlign='center' width='2' />
-        </Table.Row>
-      </Table.Body>
-    </Table>
-    <Button onClick={addNote}>Lisa noot</Button>
-    <Button onClick={() => removeNote()}>Kustuta viimane</Button>
-  </React.Fragment>
+    <div style={{paddingTop: '1rem'}}>
+      <Accordion styled active={showTable}>
+        <Accordion.Title onClick={onTitleClick} >
+          <Icon className={'chevron down ' + iconClass} id={'toggleTableIcon'} />
+        </Accordion.Title>
+        <Accordion.Content active={showTable} id={'notationTableContent'}>
+          <Table unstackable >
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell textAlign='center' width='2' />
+                <NotationTableCell name={'whole'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
+                <NotationTableCell name={'half'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
+                <NotationTableCell name={'quarter'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
+                <NotationTableCell name={'eighth'} handleClick={onNoteDurationClick} checkIfSelected={(isNoteDurationSelected)} />
+                <NotationTableCell name={'sixteenth'} handleClick={onNoteDurationClick} checkIfSelected={isNoteDurationSelected} />
+                <NotationTableCell name={'dot'} handleClick={onDotClick} checkIfSelected={isDotSelected} />
+              </Table.Row>
+              <Table.Row>
+                <NotationTableCell name={'C'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'D'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'E'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'F'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'G'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'A'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'B'} handleClick={onNoteClick} checkIfSelected={isNoteSelected} isImageCell={false} />
+                <NotationTableCell name={'rest'} handleClick={onRestClick} checkIfSelected={isRestSelected} />
+              </Table.Row>
+              <Table.Row>
+              <Table.Cell textAlign='center' width='2' />
+                <NotationTableCell name={'dblflat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
+                <NotationTableCell name={'flat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
+                <NotationTableCell name={'nat'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
+                <NotationTableCell name={'sharp'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
+                <NotationTableCell name={'dblsharp'} handleClick={onNoteAccidentalClick} checkIfSelected={isNoteAccidentalSelected} />
+                <Table.Cell textAlign='center' width='2' />
+                <Table.Cell textAlign='center' width='2' />
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell textAlign='center' width='2' />
+                <Table.Cell textAlign='center' width='2' />
+                <Table.Cell textAlign='center' width='2' />
+                <NotationTableCell name={'octaveup'} handleClick={onOctaveUpClick} popupContent={'Oktav kõrgemaks'} />
+                <NotationTableCell name={'octavedown'} handleClick={onOctaveDownClick} popupContent={'Oktav madalamaks'} />
+                <Table.Cell textAlign='center' width='2' />
+                <Table.Cell textAlign='center' width='2' />
+                <Table.Cell textAlign='center' width='2' />
+              </Table.Row>
+            </Table.Body>
+          </Table>
+          <Button onClick={addNote}>Lisa noot</Button>
+          <Button onClick={() => removeNote()}>Kustuta viimane</Button>
+        </Accordion.Content>
+      </Accordion>
+  </div>
   );
 };
 
