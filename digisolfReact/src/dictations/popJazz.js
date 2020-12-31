a1f7878c86fe3d636215f6965cc039d4b8c6ba91
Tarmo Johannes <tarmo@otsakool.edu.ee>
2020-12-31 11:45:13 +0200

    Added one popJazz dictation (no chords yet).

--- digisolfReact/src/dictations/popJazz.js
+++ digisolfReact/src/dictations/popJazz.js
@@ -19,6 +19,24 @@
             melody: ",33,3212_,23/33,333,555_,556_/1,,,", // miks lõpus 1, pekas ju 6 olema
             rhythm: "34,1234_,13,/34,123,124_,123_/1,,,/"
 
-        },
+    },
+
+     {
+     category: "popJazz", title: "Vaikida võib", soundFile: "../sounds/dictations/popJazz/level1/Vaikida_voib.mp3 ",
+     credits: "Muusika/sõnad: Ott Lepland, Oliver Rõõmus, Joonatan Siiman 0:46 - 0:50 https://youtu.be/Q46r_SVSIhk?t=46",
+     notation:
+ `
+     \\clef "treble" \\key b \\major \\time 4/4 
+    g'8 f'8 g'4 r4. g'8 | 
+    g'16 f'8. f'16 d'8. d'16 f'8. f'8 d'8 \\bar "|."            
+        `,
+     chords: [
+     {},
+     {}
+    ],
+
+ },
+
+
  
  ]
